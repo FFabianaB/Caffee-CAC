0f559ac1e6ce6d0e769c207271b673e39514949f
Fabiana Buchieri <141365085+FFabianaB@users.noreply.github.com>
2024-06-07 20:12:36 -0300

    correccion js

--- scripts/app.js
+++ scripts/app.js
@@ -34,35 +34,17 @@ if (window.scrollY === 0) {
 
 // -------------------like en productos-------------------
 
+// Seleccionar todos los iconos
+const iconos = document.querySelectorAll('.ikon');
 
-// // Selecciona todos los elementos con la clase .fa-heart
-// var icono = document.querySelectorAll('.ikon');
-
-// // Itera sobre cada icono la funcion like
-// icono.forEach(function(clikear) {
-
-//     // Agrega un evento de clickear
-//     clikear.addEventListener("click", function() {
+// Iterar sobre cada icono
+iconos.forEach(function(icono) {
+    // Agregar un evento de clic a cada icono
+    icono.addEventListener("click", function() {
+        // Seleccionar el corazón dentro del icono actual
+        const corazon = icono.querySelector('.fa-heart');
         
-//         // Añade la clase 'red' al elemento que fue clickeado
-//         var ai= document.getElementById('kokoro')
-//         ai.classList.toggle('red');
-//     })
-//  });
-
-
-// Elemento icono
-const icono = document.querySelectorAll('.ikon');
-// Elemento corazon
-const corazon= document.querySelectorAll('.fa-heart');
-
-// Para cada icono funcion like
-corazon.forEach(function(like) {
-    //  En icono escucha click
-    like.addEventListener("click", function() {
+            corazon.classList.toggle('red');
         
-        // agrega y quita clase red en 
-        like.classList.toggle('red');
-       
-    })
- });
+    });
+});
