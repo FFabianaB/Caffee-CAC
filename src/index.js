9e7de5073dc5e8e666f4d0c94633ed6ad605f621
Fabiana Buchieri <141365085+FFabianaB@users.noreply.github.com>
2024-06-16 23:47:21 -0300

    rutas relativas

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const app = express();
 // Middleware para servir archivos estáticos en la carpeta 'public'
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Importar las rutas
-const indexRouter = require('/routes/index.js');
-const sectionsRouter = require('/routes/sections.js');
+// Importar las rutas relativas
+const indexRouter = require('./routes/index');
+const sectionsRouter = require('./routes/sections');
 
 // Usar las rutas
 app.use('/', indexRouter);
-app.use('/', sectionsRouter);
+app.use('/sections', sectionsRouter);
 
 // Exportar la aplicación para Vercel
 module.exports = app;
